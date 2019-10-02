18f96eaff0e9468945e32883919a93f8d6e3e071
lilu <lilugirl2005@126.com>
2019-10-02 15:23:07 +0800

    使用file-loader打包图片文件

---
+++ 7/src/index.js
@@ -0,0 +1,8 @@
+import Header from './header';
+import Sidebar from './sidebar';
+import Content from './content';
+var avatar = require('./avatar.jpg');
+console.log(avatar);
+new Header();
+new Sidebar();
+new Content();
